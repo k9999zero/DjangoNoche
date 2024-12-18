87160d60eb13a72b319a58798abe7b17383db6a4
GojanDed <125888295+GojanDEd@users.noreply.github.com>
2024-12-18 02:19:42 -0400

    category_name add

--- appPrueba/serializers.py
+++ appPrueba/serializers.py
@@ -3,38 +3,38 @@ from django.core.validators import EmailValidator
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from .models import Product,Category
+from .models import Product, Category
 from django.contrib.auth import authenticate
-
 from django.contrib.auth import get_user_model
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']  # Incluir solo los campos que necesitas de Category
 
 class ProductSerializer(serializers.ModelSerializer):
-    #category = CategorySerializer()  # Incluir la categoría como un objeto anidado (opcional)
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    category_name = serializers.SerializerMethodField()  # Campo adicional para el nombre de la categoría
 
     class Meta:
         model = Product
-        fields = ['id', 'category', 'name', 'price', 'image','description']
+        fields = ['id', 'category', 'category_name', 'name', 'price', 'image', 'description']
 
+    def get_category_name(self, obj):
+        return obj.category.name if obj.category else None  # Retorna el nombre de la categoría
 
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True, error_messages= {
-    'required': 'El correo electronico es un campo requerido',
-    'invalid': 'Correo electronico inválido',
-    'blank': 'El correo electronico es un campo requerido',
-})
+    email = serializers.EmailField(required=True, error_messages={
+        'required': 'El correo electronico es un campo requerido',
+        'invalid': 'Correo electronico inválido',
+        'blank': 'El correo electronico es un campo requerido',
+    })
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        # Obtener el email y la contraseña del usuario
         email = data.get('email')
         password = data.get('password')
 
-        # Validar que ambos campos están presentes
         if not email or not password:
             raise serializers.ValidationError("Email and password are required.")
 
@@ -49,16 +49,15 @@ class LoginSerializer(serializers.Serializer):
         if not user:
             raise serializers.ValidationError({"auth_error": "Invalid credentials."})
 
-        # Verificar si el usuario está activo
         if not user.is_active:
-            raise serializers.ValidationError({"auth_error":"User account is disabled."})
+            raise serializers.ValidationError({"auth_error": "User  account is disabled."})
 
-        # Añadir el usuario al contexto para usarlo más adelante
         data['user'] = user
         return data
 
 User  = get_user_model()
+
 class UserSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = User
-            fields = ['id', 'username', 'email', 'first_name', 'last_name']  # Add any other fields you need
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']  # Add any other fields you need
